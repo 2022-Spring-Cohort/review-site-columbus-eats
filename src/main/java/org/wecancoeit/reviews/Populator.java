d0939602f3e33869a19199ca39a0bb3c00ac8e70
EVEN <evensalew07@yahoo.com>
2022-02-21 15:45:12 -0500

    African Populator update

--- src/main/java/org/wecancoeit/reviews/Populator.java
+++ src/main/java/org/wecancoeit/reviews/Populator.java
@@ -23,12 +23,19 @@ public class Populator implements CommandLineRunner {
     public void run(String... args) throws Exception {
         FoodGenre seafood= new FoodGenre("seafood","stuff from the ocean","/img/cajun.png");
         foodGenreRepo.save(seafood);
-
+        FoodGenre Africanfood= new FoodGenre("Africanfood","stuff from back home","/img/AfricanLogo.png");
+        foodGenreRepo.save(Africanfood);
 
         Restaurant restaurants1 = new Restaurant("the Cajun",seafood,"Taste the south with authentic low country boil style cooking");
         Restaurant restaurants2 = new Restaurant("red lobster", seafood,"mmmmmm biscuits");
+        Restaurant restaurants5 = new Restaurant("Lalibela",Africanfood,"Lamb");
+        Restaurant restaurants6 = new Restaurant("HabeshaGarden",Africanfood,"goatmeat");
+
         restaurantRepo.save(restaurants1);
         restaurantRepo.save(restaurants2);
+        restaurantRepo.save(restaurants5);
+        restaurantRepo.save(restaurants6);
+
 
 
         Review review1 = new Review("Arjin Harris","arjinh","best food ever",5,"/img/logo.png",restaurants1);
