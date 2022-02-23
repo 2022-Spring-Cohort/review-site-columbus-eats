305f83f802890e9ba08381a728bedd3d69ad616f
arjinharris1 <ARJINHARRIS@GMAIL.COM>
2022-02-23 13:53:15 -0500

    restaurant links

--- src/main/java/org/wecancoeit/reviews/repo/FoodGenreRepo.java
+++ src/main/java/org/wecancoeit/reviews/repo/FoodGenreRepo.java
@@ -2,7 +2,10 @@ package org.wecancoeit.reviews.repo;
 
 import org.springframework.data.repository.CrudRepository;
 import org.wecancoeit.reviews.entities.FoodGenre;
+import org.wecancoeit.reviews.entities.Restaurant;
+
+import java.util.Optional;
 
 public interface FoodGenreRepo extends CrudRepository<FoodGenre,Long> {
-
+//    Optional<FoodGenre> findByFoodGenre(FoodGenre foodGenre);
 }
