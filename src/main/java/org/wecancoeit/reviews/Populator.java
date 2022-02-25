2bfeac11105eeaa7181951772a67b4ff214e2d6c
EVEN <evensalew07@yahoo.com>
2022-02-25 10:52:56 -0500

    Edit o populator

--- src/main/java/org/wecancoeit/reviews/Populator.java
+++ src/main/java/org/wecancoeit/reviews/Populator.java
@@ -26,7 +26,7 @@ public class Populator implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        FoodGenre seafood = new FoodGenre("seafood", "Seafood is any form of sea life regarded as food by humans, prominently including fish and shellfish. Shellfish include various species of molluscs, crustaceans, and echinoderms.", "/img/newSeaLogoGenre.jpg");
+        FoodGenre seafood = new FoodGenre("Seafood", "Seafood is any form of sea life regarded as food by humans, prominently including fish and shellfish. Shellfish include various species of molluscs, crustaceans, and echinoderms.", "/img/newSeaLogoGenre.jpg");
         foodGenreRepo.save(seafood);
         FoodGenre Africanfood = new FoodGenre("African", "The various cuisines of Africa use a combination of locally available fruits, cereal grains and vegetables, as well as milk and meat products.", "/img/AfricanLogo.png");
         foodGenreRepo.save(Africanfood);
@@ -36,12 +36,12 @@ public class Populator implements CommandLineRunner {
         FoodGenre Mexican = new FoodGenre("Mexican", "Cuisines consists of the cooking cuisines and traditions of the modern country of Mexico.", "/img/mexGenre.png");
         foodGenreRepo.save(Mexican);
 
-        Hashtag hashtag1 = new Hashtag("Spicy","burns the tongue");
+        Hashtag hashtag1 = new Hashtag("Spicy","Burns the tongue");
         Hashtag hashtag2 = new Hashtag("Vegetarian","Vegetarian friendly restaurant");
-        Hashtag hashtag = new Hashtag("Sweets","dessert available");
-        Hashtag hashtag3 = new Hashtag("kidMenu","stuff for the little ones");
-        Hashtag hashtag4 = new Hashtag("Bar","get your drink on");
-        Hashtag hashtag5 = new Hashtag("Ethnic","good minority eats");
+        Hashtag hashtag = new Hashtag("Sweets","Dessert available");
+        Hashtag hashtag3 = new Hashtag("KidsMenu","Stuff for the little ones");
+        Hashtag hashtag4 = new Hashtag("Bar","Get your drink on");
+        Hashtag hashtag5 = new Hashtag("Ethnic","Good minority eats");
         hashtagRepo.save(hashtag);
         hashtagRepo.save(hashtag1);
         hashtagRepo.save(hashtag2);
@@ -50,14 +50,14 @@ public class Populator implements CommandLineRunner {
         hashtagRepo.save(hashtag5);
 
 
-        Restaurant restaurants1 = new Restaurant("the Cajun", seafood, "Taste the south with authentic low country boil style cooking", "https://www.cajunboilbar.com/","img/cajunboil.png", hashtag);
-        Restaurant restaurants2 = new Restaurant("red lobster", seafood, "mmmmmm biscuits", "https://www.redlobster.com/","img/redLobster.png", hashtag1);
-        Restaurant restaurants3 = new Restaurant("Lalibela Ethiopian Restaurant", Africanfood, "Lamb", "https://lalibelarestaurant.net/","img/africanPic2.png/",hashtag4);
-        Restaurant restaurants4 = new Restaurant("Hoyo's Kitchen", Africanfood, "goatmeat", "http://hoyoskitchen.com/","img/Somali Restaurant.jpg",hashtag3);
-        Restaurant restaurants5 = new Restaurant("Lan Viet", Asian, "Filler Filler Filler", "https://lanvietmarket.com/","img/lanviet.jpg/",hashtag4, hashtag1);
-        Restaurant restaurants6 = new Restaurant("Gogi Korean BBQ", Asian, "Filler Filler Filler", "https://gogikbbq.com/","img/Gogi.png/",hashtag,hashtag1,hashtag3);
-        Restaurant restaurants7 = new Restaurant("Local Cantina", Mexican, "Filler Filler Filler", "https://localcantina.com/","img/local-cantina-logo.png/",hashtag1,hashtag3);
-        Restaurant restaurants8 = new Restaurant("El Vaquero", Mexican, "Filler Filler Filler", "https://www.mexicanrestauranthilliard.com/","img/elVaquero.jpg/",hashtag5,hashtag3);
+        Restaurant restaurants1 = new Restaurant("The Cajun", seafood, "Taste the south with authentic low country boil style cooking", "https://www.cajunboilbar.com/","img/cajunboil.png", hashtag4,hashtag1);
+        Restaurant restaurants2 = new Restaurant("Red Lobster", seafood, "", "https://www.redlobster.com/","img/redLobster.png", hashtag1,hashtag4);
+        Restaurant restaurants3 = new Restaurant("Lalibela Ethiopian Restaurant", Africanfood, "-are served a vast array of foods, ranging from multicolored mounds of spicy stews to vegetable curries to cubes of raw ", "https://lalibelarestaurant.net/","img/africanPic2.png/",hashtag2,hashtag5,hashtag4);
+        Restaurant restaurants4 = new Restaurant("Hoyo's Kitchen", Africanfood, "", "http://hoyoskitchen.com/","img/Somali Restaurant.jpg",hashtag1,hashtag5,hashtag);
+        Restaurant restaurants5 = new Restaurant("Lan Viet", Asian, "", "https://lanvietmarket.com/","img/lanviet.jpg/",hashtag5, hashtag1);
+        Restaurant restaurants6 = new Restaurant("Gogi Korean BBQ", Asian, "", "https://gogikbbq.com/","img/Gogi.png/",hashtag5,hashtag1,hashtag3);
+        Restaurant restaurants7 = new Restaurant("Local Cantina", Mexican, "", "https://localcantina.com/","img/local-cantina-logo.png/",hashtag1,hashtag,hashtag5);
+        Restaurant restaurants8 = new Restaurant("El Vaquero", Mexican, "", "https://www.mexicanrestauranthilliard.com/","img/elVaquero.jpg/",hashtag5,hashtag1);
 
 
         restaurantRepo.save(restaurants1);
@@ -71,15 +71,31 @@ public class Populator implements CommandLineRunner {
 
         Review review1 = new Review("Arjin Harris", "arjinh", "Best food ever", 5, "/img/logo.png", restaurants1);
         Review review2 = new Review("Kevin Ha", "crayonEater", "It was good food but not as good as the red crayon", 4, "/img/logo.png", restaurants2);
+        Review review3 = new Review("Peggy Hill","theHillDog","I really Like asian food but gogi is next level although it is a little expensive",4,"/img/logo.png",restaurants6);
+        Review review4 = new Review("James bond","007","They gave me a martini shakened, not stirred", 4,"/img/logo.png",restaurants4);
+        Review review5 = new Review("Arjin Harris", "arjinh", "best food ever", 5, "/img/logo.png", restaurants1);
+        Review review6 = new Review("Kevin Ha", "crayonEater", "mmmmmmmmm biscuits, but not as good as the red crayon", 3, "/img/logo.png", restaurants2);
+        Review review7 = new Review("Peggy Hill","theHillDog","I really Like asian food but gogi is next level although it is a little expensive",4,"/img/logo.png",restaurants6);
+        Review review8 = new Review("James bond","007","They gave me a martini shakened, not stirred", 4,"/img/logo.png",restaurants4);
 
-
+        reviewRepo.save(review4);
         reviewRepo.save(review2);
         reviewRepo.save(review1);
-
+        reviewRepo.save(review3);
+        reviewRepo.save(review5);
+        reviewRepo.save(review6);
+        reviewRepo.save(review7);
+        reviewRepo.save(review8);
+        restaurants4.addReview(review4);
+        restaurants6.addReview(review6);
         restaurants1.addReview(review1);
-        restaurants1.addReview(review2);
-        restaurants2.addReview(review1);
         restaurants2.addReview(review2);
+        restaurants3.addReview(review3);
+        restaurants5.addReview(review5);
+        restaurants7.addReview(review7);
+        restaurants8.addReview(review8);
+
+
 
 
 
